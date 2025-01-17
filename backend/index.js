1e415d99aaa1722e6e615e8bab5878d8d75cfd46
sagar252004 <132927520+sagar252004@users.noreply.github.com>
2025-01-17 23:35:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 
 
 const corsOptions = {
-    origin:   'http://localhost:5173',
+    origin:   'https://gain-guru-seven.vercel.app/',
      // Allow only your frontend
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
     credentials: true, // If you need to send cookies or authorization headers
